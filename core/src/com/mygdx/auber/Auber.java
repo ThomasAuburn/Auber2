d008125c330013af6f9fd25e383dfcaa1795b589
Lakhan Mankani <lakhan.mankani@gmail.com>
2020-10-18 20:13:41 +0100

    Add userCharacter to Auber

--- core/src/com/mygdx/auber/Auber.java
+++ core/src/com/mygdx/auber/Auber.java
@@ -10,45 +10,46 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Auber extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture texture;
 	Sprite room;
-	Sprite character;
+	Character userCharacter;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		texture = new Texture("Room 1.png");
-		character = new Sprite(new Texture("Construction worker.png"));
-		room = new Sprite(texture);
+		room = new Sprite(new Texture("Room 1.png"));
+		room.setPosition(Gdx.graphics.getWidth()/2f - room.getWidth()/2,
+				Gdx.graphics.getHeight()/2f - room.getHeight()/2);
+		room.scale(0.1f);
+
+		userCharacter = new Character("Construction worker.png", batch);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			character.translate(-5, 0);
+			userCharacter.translate(-5, 0);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			character.translate(5, 0);
+			userCharacter.translate(5, 0);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-			character.translate(0, 5);
+			userCharacter.translate(0, 5);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-			character.translate(0, -5);
+			userCharacter.translate(0, -5);
 		}
 
 		batch.begin();
 		room.draw(batch);
-		character.draw(batch);
+		userCharacter.sprite.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		texture.dispose();
 	}
 }
