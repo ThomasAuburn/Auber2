b5791891396e5346d88e3f58cf24e444d818e4f9
Lakhan Mankani <lakhan.mankani@gmail.com>
2020-10-18 13:09:06 +0100

    Initial commit

---
+++ desktop/src/com/mygdx/auber/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.auber.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.auber.Auber;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Auber(), config);
+	}
+}
