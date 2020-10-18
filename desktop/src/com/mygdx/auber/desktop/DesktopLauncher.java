ede9b256455128cabcb92f65c426fe37970ff7ef
Lakhan Mankani <lakhan.mankani@gmail.com>
2020-10-18 18:19:16 +0100

    Set window size 1024x1024

--- desktop/src/com/mygdx/auber/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/auber/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.auber.Auber;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 1024;
 		new LwjglApplication(new Auber(), config);
 	}
 }
