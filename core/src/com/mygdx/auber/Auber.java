d71b5041ab058e487d6a777a1d11ab6be112542a
Lakhan Mankani <lakhan.mankani@gmail.com>
2020-10-18 17:44:43 +0100

    Add sprites to auber

--- core/src/com/mygdx/auber/Auber.java
+++ core/src/com/mygdx/auber/Auber.java
@@ -5,15 +5,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Auber extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture texture;
+	Sprite sprite;
+	Sprite character;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		texture = new Texture("Room 1.png");
+		character = new Sprite(new Texture("Construction worker.png"));
+		sprite = new Sprite(texture);
 	}
 
 	@Override
@@ -21,13 +26,14 @@ public class Auber extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
+		character.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		texture.dispose();
 	}
 }
