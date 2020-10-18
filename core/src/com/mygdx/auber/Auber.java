2e6f5a8389072f85226002d6e851c2a2d20c2ac8
Lakhan Mankani <lakhan.mankani@gmail.com>
2020-10-18 17:46:28 +0100

    Add arrow keys input movement of character

--- core/src/com/mygdx/auber/Auber.java
+++ core/src/com/mygdx/auber/Auber.java
@@ -2,6 +2,7 @@ package com.mygdx.auber;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -25,6 +26,20 @@ public class Auber extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			character.translate(-5, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			character.translate(5, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			character.translate(0, 5);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			character.translate(0, -5);
+		}
+
 		batch.begin();
 		sprite.draw(batch);
 		character.draw(batch);
