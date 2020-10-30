363d64ae42af415b08d79aa4595f148c64adf2f4
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2020-10-30 17:59:14 +0000

    Fixed top collision detection

--- desktop/src/com/mygdx/auber/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/auber/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.auber.Auber;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.foregroundFPS = 144;
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new Auber(), config);
 	}
 }
