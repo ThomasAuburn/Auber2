595d1faaef4f8dea7198a2322c2527ee3da191d2
EHigggins <ethanjhiggins7@gmail.com>
2020-11-12 14:15:07 +0000

    npc and infiltrator

---
+++ core/src/com/mygdx/auber/Screens/MainMenuScreen.java
@@ -0,0 +1,77 @@
+package com.mygdx.auber.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.auber.Auber;
+
+public class MainMenuScreen implements Screen {
+
+    private Viewport viewport;
+    private Stage stage;
+
+    private Auber game;
+
+    public MainMenuScreen(Auber game){
+        this.game = game;
+        viewport = new ExtendViewport(Auber.VirtualWidth, Auber.VirtualHeight, new OrthographicCamera());
+        stage = new Stage(viewport, ((Auber) game).batch);
+
+        Label.LabelStyle font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
+
+        Table table = new Table();
+        table.center();
+        table.setFillParent(true);
+
+        Label gameOverLabel = new Label("MAIN MENU TEMP", font);
+        table.add(gameOverLabel).expandX();
+
+        stage.addActor(table);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
