407e0837c40ede0ac9dde21ca6316662d20ce1e6
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2020-11-13 18:11:15 +0000

    Commented on pathfinding classes

--- core/src/com/mygdx/auber/Pathfinding/GraphCreator.java
+++ core/src/com/mygdx/auber/Pathfinding/GraphCreator.java
@@ -30,6 +30,9 @@ public class GraphCreator extends ApplicationAdapter {
         generateConnections();
     }
 
+    /**
+     * Renders the nodes and paths on the screen using their render methods, used for debugging purposes
+     */
     @Override
     public void render() {
 
@@ -52,35 +55,45 @@ public class GraphCreator extends ApplicationAdapter {
     {
         for (int i = 0; i < tileLayer.getWidth(); i++)
         {
-            for(int j = 0; j < tileLayer.getHeight(); j++)
+            for(int j = 0; j < tileLayer.getHeight(); j++) //Scans every tile
             {
-                int x = i * tileLayer.getTileWidth();
-                int y = j * tileLayer.getTileHeight();
-                TiledMapTileLayer.Cell cell = tileLayer.getCell(i, j);
-                if(cell != null && cell.getTile() != null && (cell.getTile().getId() == 11 || cell.getTile().getId() == 12))
+                int x = (i * tileLayer.getTileWidth()) + tileLayer.getTileWidth()/2;
+                int y = (j * tileLayer.getTileHeight()) - tileLayer.getTileHeight()/2; //x,y coord of the centre of the tile
+                TiledMapTileLayer.Cell cell = tileLayer.getCell(i, j); //Returns the cell at the x,y coord
+                if(cell != null && cell.getTile() != null && (cell.getTile().getId() == 11 || cell.getTile().getId() == 12)) //If ID matches floor/corridor tiles, and is not null
                 {
                     Node node = new Node(x,y);
-                    MapGraph.addNode(node);
+                    MapGraph.addNode(node); //Create new node and add it to the map graph
                 }
             }
         }
     }
-    
+
+    /**
+     * Generates a path from each node to its neighbour nodes (up, down, left, right)
+     * Called when GraphCreator is instantiated, after the nodes are created
+     */
     public void generateConnections()
     {
         for (int i = 0; i < MapGraph.nodes.size; i++)
         {
             Node node = MapGraph.nodes.get(i);
-            Array<Node> neighbourNodes = getNeighbourNodes(node);
+            Array<Node> neighbourNodes = getNeighbourNodes(node); //For every node in the map, get its neighbours
             for (Node neighbourNode: neighbourNodes)
             {
-                MapGraph.connectNodes(node, neighbourNode);
+                MapGraph.connectNodes(node, neighbourNode); //For each neighbour, connect it to the original node
             }
         }
     }
 
+    /**
+     * Returns every valid neighbour node to the node passed
+     * @param node Node to get neighbours for
+     * @return Array of Nodes
+     */
     public Array<Node> getNeighbourNodes(Node node)
     {
+        //Creates an array of each valid (not null) neighbour node and returns it
         Array<Node> nodes = new Array<>();
         float x = node.x;
         float y = node.y;
--- core/src/com/mygdx/auber/Pathfinding/Node.java
+++ core/src/com/mygdx/auber/Pathfinding/Node.java
@@ -15,22 +15,35 @@ public class Node {
         this.y = y;
     }
 
+    /**
+     * Setter for the index of the node, used for A* Indexed search
+     * @param index The index to assign the node
+     */
     public void setIndex(int index)
     {
         this.index = index;
     }
 
+    /**
+     * Renderer for the nodes, used for debugging purposes
+     * @param shapeRenderer The shape renderer being used
+     * @param batch The sprite batch being used
+     * @param font The font to use
+     */
     public void render(ShapeRenderer shapeRenderer, SpriteBatch batch, BitmapFont font){
+        //Drawing the Circle which holds the node
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(.8f, .88f, .95f, 1);
         shapeRenderer.circle(x * 16, y * 16, 10);
         shapeRenderer.end();
 
+        //Drawing the outline of the circle
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(0, 0, 0, 1);
         shapeRenderer.circle(x * 16, y * 16, 10);
         shapeRenderer.end();
 
+        //Writing the nodes index in the circle
         batch.begin();
         font.setColor(0, 0, 0, 255);
         font.draw(batch, Integer.toString(index), x  * 16, y  * 16);
