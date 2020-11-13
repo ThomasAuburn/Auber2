ef948a4dbed8cd03f7394bc2c4393018ce0a01f4
Lakhan Mankani <lakhan.mankani@gmail.com>
2020-11-13 19:43:30 +0000

    Add documentation to Collision class

--- core/src/com/mygdx/auber/entities/Collision.java
+++ core/src/com/mygdx/auber/entities/Collision.java
@@ -11,12 +11,11 @@ class Collision {
     }
 
     /**
-     * "Function that scans the blocks directly right of the sprite, for the height of the sprite, and returns a bool based on if they are blocked or not"
+     * Scans the blocks directly right of the sprite, for the height of the sprite, and returns a bool based on if they are blocked or not
      *
-     * Args: None
-     *
-     * Returns:
-     *  bool: True if there is a on the right of the sprite that contains "blocked", else returns false
+     * @param sprite A Sprite object of the character
+     * @param collisionLayer Collision layer in the Tiled map
+     * @return True if there is a on the right of the sprite that contains "blocked", else returns false
      */
     public boolean collidesRight(Sprite sprite, TiledMapTileLayer collisionLayer) {
         // Iterate across the amount of tiles tall the sprite is
@@ -29,8 +28,15 @@ class Collision {
         return false;
     }
 
+    /**
+     * Scans the blocks directly left of the sprite, for the height of the sprite, and returns a bool based on if they are blocked or not
+     *
+     * @param sprite A Sprite object of the character
+     * @param collisionLayer Collision layer in the Tiled map
+     * @return True if there is a on the left of the sprite that contains "blocked", else returns false
+     */
     public boolean collidesLeft(Sprite sprite, TiledMapTileLayer collisionLayer) {
-        // Iterate across the amount of tiles tall the sprite is
+        // Iterate across the amount of tiles wide the sprite is
         for (float step = collisionLayer.getTileHeight() / 2f; step < sprite.getHeight(); step += collisionLayer.getTileHeight() / 2f) {
             // Check if cell contains blocked property
             boolean collides = isCellBlocked(collisionLayer, sprite.getX() , sprite.getY() + step);
@@ -40,8 +46,15 @@ class Collision {
         return false;
     }
 
+    /**
+     * Scans the blocks directly top of the sprite, for the height of the sprite, and returns a bool based on if they are blocked or not
+     *
+     * @param sprite A Sprite object of the character
+     * @param collisionLayer Collision layer in the Tiled map
+     * @return True if there is a on the top of the sprite that contains "blocked", else returns false
+     */
     public boolean collidesTop(Sprite sprite, TiledMapTileLayer collisionLayer) {
-        // Iterate across the amount of tiles tall the sprite is
+        // Iterate across the amount of tiles wide the sprite is
         for (float step = collisionLayer.getTileWidth() / 2f; step < sprite.getWidth(); step += collisionLayer.getTileWidth() / 2f) {
             // Check if cell contains blocked property
             boolean collides = isCellBlocked(collisionLayer, sprite.getX() + step, sprite.getY() + sprite.getHeight());
@@ -51,6 +64,13 @@ class Collision {
         return false;
     }
 
+    /**
+     * Scans the blocks directly bottom of the sprite, for the height of the sprite, and returns a bool based on if they are blocked or not
+     *
+     * @param sprite A Sprite object of the character
+     * @param collisionLayer Collision layer in the Tiled map
+     * @return True if there is a on the bottom of the sprite that contains "blocked", else returns false
+     */
     public boolean collidesBottom(Sprite sprite, TiledMapTileLayer collisionLayer) {
         // Iterate across the amount of tiles tall the sprite is
         for (float step = collisionLayer.getTileWidth()/2f; step < sprite.getWidth(); step += collisionLayer.getTileWidth()/2f) {
@@ -63,14 +83,12 @@ class Collision {
     }
 
     /**
-     * "A function that checks a cell and returns a bool for whether the cell contains the key "blocked" or not
+     * "Checks a cell and returns a bool for whether the cell contains the key "blocked" or not
      *
-     * Args:
-     *      x (float): X coordinate of the cell to check
-     *      y(float): Y coordinate of the cell to check
-     *
-     * Returns:
-     *      bool: True if cell contains "blocked", else false
+     * @param collisionLayer Collision layer in the Tiled map
+     * @param x X coordinate of the cell to check
+     * @param y Y coordinate of the cell to check
+     * @return True if cell contains "blocked", else false
      */
     private boolean isCellBlocked(TiledMapTileLayer collisionLayer, float x,float y) {
         TiledMapTileLayer.Cell cell = collisionLayer.getCell((int) (x / collisionLayer.getTileWidth()), (int) (y / collisionLayer.getTileHeight())); //Set variable cell to the cell at specified x,y coordinate
