90f17d0eee685f98fae96f0cda66c7626fb2ea02
EHigggins <ethanjhiggins7@gmail.com>
2020-11-17 18:08:13 +0000

    dogshit main menu that doesnt work like i want it to

--- core/src/com/mygdx/auber/Screens/MainMenuScreen.java
+++ core/src/com/mygdx/auber/Screens/MainMenuScreen.java
@@ -1,53 +1,84 @@
 package com.mygdx.auber.Screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.auber.Auber;
+import com.mygdx.auber.ScrollingBackground;
+
 
 public class MainMenuScreen implements Screen {
 
     private Viewport viewport;
-    private Stage stage;
-
+    Stage stage;
+    TextButton playButton, exitButton;
+    TextButton.TextButtonStyle textButtonStyle;
+    BitmapFont font;
+    Skin skin;
+    TextureAtlas buttonAtlas;
     private Auber game;
+    private ScrollingBackground scrollingBackground;
+    private Screen PlayScreen;
 
-    public MainMenuScreen(Auber game){
+    public MainMenuScreen(final Auber game){
         this.game = game;
+
         viewport = new ExtendViewport(Auber.VirtualWidth, Auber.VirtualHeight, new OrthographicCamera());
         stage = new Stage(viewport, ((Auber) game).batch);
 
-        Label.LabelStyle font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
+        font = new BitmapFont();
+        skin = new Skin();
+        buttonAtlas = new TextureAtlas("buttonAtlas.atlas");
+        skin.addRegions(buttonAtlas);
+        textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("up-button");
+        textButtonStyle.down = skin.getDrawable("down-button");
+        textButtonStyle.checked = skin.getDrawable("checked-button");
+        playButton = new TextButton("PLAY", textButtonStyle);
+        exitButton = new TextButton("QUIT", textButtonStyle);
+        playButton.setSize(200, 200);
 
-        Table table = new Table();
-        table.center();
-        table.setFillParent(true);
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Clicked");
+                game.setScreen(PlayScreen);
+            }
+        });
 
-        Label gameOverLabel = new Label("MAIN MENU TEMP", font);
-        table.add(gameOverLabel).expandX();
+        Table menuTable = new Table();
+        menuTable.setTouchable(Touchable.enabled);
+        menuTable.setFillParent(true);
+        menuTable.add(playButton).padBottom(20);
+        menuTable.row();
+        menuTable.add(exitButton);
+        menuTable.debug();
 
-        stage.addActor(table);
+        stage.addActor(menuTable);
     }
 
     @Override
     public void show() {
 
+
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         stage.draw();
+
+
     }
 
     @Override
@@ -72,6 +103,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
+
 
     }
 }
